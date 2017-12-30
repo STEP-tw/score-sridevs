6a425154b6c689cb9739248d98570ccab6e8f1d7
sridevs <senthilnathansridev@gmail.com>
2017-12-30 15:09:33 +0530

    added score constructer with getScore and updateScore methods

---
+++ src/score.js
@@ -0,0 +1,11 @@
+const Score = function () {
+  this.score = 0;
+};
+
+Score.prototype.getScore = function () {
+  return this.score;
+};
+
+Score.prototype.updateScore = function () {}
+  return this.score += 10;
+};
